ac23442c673a6e4e7203859cfe4ccb52d3a4d68f
David PR <serallapdivad@gmail.com>
2023-11-26 21:10:02 +0000

    Added new section

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css'
 import Intro from './components/Intro'
 import ProjectContent from './components/ProjectContent'
 import ContactContent from './components/ContactContent'
+import AboutMeContent from './components/AboutMeContent'
 
 const App: FC = () => {
 	return (
 		<>
 			<Intro />
+			<AboutMeContent />
 			<ProjectContent />
 			<ContactContent />
 		</>
