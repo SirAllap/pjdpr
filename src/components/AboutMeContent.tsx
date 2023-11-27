deb22b6b3fc802f072a353f1faba3193d3ebb3f9
David PR <serallapdivad@gmail.com>
2023-11-27 23:50:51 +0000

    update aboutMe section removing unused text

--- src/components/AboutMeContent.tsx
+++ src/components/AboutMeContent.tsx
@@ -1,13 +1,11 @@
 import { FC } from 'react'
 import styled from 'styled-components'
-import { NormalText } from '../shared_styling/Styled'
 import UserCard from './UserCard'
 
 const AboutMeContent: FC = () => {
 	return (
 		<>
 			<BodyContainer id='aboutMeSection'>
-				<NormalText>Contact info</NormalText>
 				<UserCard />
 			</BodyContainer>
 		</>
