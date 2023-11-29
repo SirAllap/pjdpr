f2f6e10a2719756473406072317ce79e75189402
David PR <serallapdivad@gmail.com>
2023-11-29 19:57:41 +0000

    removed the about section

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import './App.css'
 import Intro from './components/Intro'
 import ProjectContent from './components/ProjectContent'
 import ContactContent from './components/ContactContent'
-import AboutMeContent from './components/AboutMeContent'
 
 const App: FC = () => {
 	return (
 		<>
 			<Intro />
-			<AboutMeContent />
 			<ProjectContent />
 			<ContactContent />
 		</>
