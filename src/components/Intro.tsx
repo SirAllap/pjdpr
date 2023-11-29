41e50daa02be540ed282a59edac18f37c1fabb6d
David PR <serallapdivad@gmail.com>
2023-11-29 20:01:56 +0000

    Intro: - Removed the dark mode icon button - Improved the general layout

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,62 +1,37 @@
 import { FC, useEffect, useState } from 'react'
 import styled from 'styled-components'
 // import UserCard from './UserCard'
-import { BigTitle, CTA, Links, NormalText } from '../shared_styling/Styled'
-import { DarkModeSwitch } from 'react-toggle-dark-mode'
+import { BigTitle, CTA, Links } from '../shared_styling/Styled'
 import imageMe from '../assets/me1.png'
+import UserCard from './UserCard'
 
 const Intro: FC = () => {
 	const [scrollY, setScrollY] = useState(0)
-	const [shadowed, setShadowed] = useState(false)
-	const [isDarkMode, setIsDarkMode] = useState(
-		window.matchMedia('(prefers-color-scheme: dark)').matches
-	)
-	const [isLigthMode, setIsLigthMode] = useState(
-		window.matchMedia('(prefers-color-scheme: ligth)').matches
-	)
-	const toggleDarkMode = () => {
-		setIsLigthMode(!isLigthMode), setIsDarkMode(!isDarkMode)
-	}
+	const [shadowed, setShadowed] = useState('no')
 	function logit() {
 		setScrollY(window.scrollY)
 	}
 
 	useEffect(() => {
-		document.documentElement.classList.toggle('dark-mode', isDarkMode)
-		document.documentElement.classList.toggle('ligth-mode', isLigthMode)
 		function watchScroll() {
 			window.addEventListener('scroll', logit)
 		}
 		watchScroll()
-		scrollY >= 100 ? setShadowed(true) : setShadowed(false)
+		scrollY >= 100 ? setShadowed('yes') : setShadowed('no')
 		return () => {
 			window.removeEventListener('scroll', logit)
 		}
-	}, [isDarkMode, isLigthMode, scrollY])
+	}, [scrollY])
 	return (
 		<>
-			<DarkModeSwapper>
-				<DarkModeSwitch
-					checked={isDarkMode}
-					onChange={toggleDarkMode}
-					size={70}
-					moonColor={'white'}
-					sunColor={'white'}
-				/>
-			</DarkModeSwapper>
 			<Links href='https://github.com/SirAllap' target='_blank'>
-				<CTAGH>
-					<NormalText>GitHub</NormalText>
-				</CTAGH>
+				<CTAGH>GitHub</CTAGH>
 			</Links>
 			<Container>
 				<HeaderContainer shadow={shadowed}>
 					<LeftHeaderTextContainer>
 						<Links href='#projectSection'>
-							<NormalText textSize='subTitle'>WORK</NormalText>
-						</Links>
-						<Links href='#aboutMeSection'>
-							<NormalText textSize='subTitle'>ABOUT</NormalText>
+							<CTA>WORK</CTA>
 						</Links>
 					</LeftHeaderTextContainer>
 					<Links href='#'>
@@ -64,170 +39,180 @@ const Intro: FC = () => {
 					</Links>
 					<RightHeaderTextContainer>
 						<Links href='#contactSection'>
-							<CTA>Hit me up</CTA>
+							<CTA>Contact</CTA>
 						</Links>
 					</RightHeaderTextContainer>
 				</HeaderContainer>
-				<MePicture>
+				<MePicture />
+				<TextArea>
 					<IntroContainer>Hi. I'm David.</IntroContainer>
 					<IntroNextContainer>A Developer.</IntroNextContainer>
-					<Arrow href='#aboutMeSection'></Arrow>
-				</MePicture>
+					<UserCard />
+				</TextArea>
 			</Container>
+			<Arrow href='#projectSection' />
 		</>
 	)
 }
 
 const Container = styled.div`
 	position: relative;
-	height: 100vh;
+	height: 100dvh;
+`
+
+const TextArea = styled.div`
+	width: 60%;
+	height: fit-content;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	@media (max-width: 600px) {
+		top: 27%;
+		width: 100%;
+	}
 `
 
 const IntroContainer = styled.div`
-	position: absolute;
-	left: 50%;
-	top: 50%;
-	transform: translate(-50%, -50%);
 	text-align: center;
-	font-size: 7vw;
-	width: 62%;
-`
-const IntroNextContainer = styled.div`
-	position: relative;
-	left: 50%;
-	top: 62%;
-	transform: translate(-50%, -40%);
-	text-align: center;
-	font-size: 7vw;
-	width: 62%;
+	font-size: 7rem;
+	text-transform: uppercase;
 	transition: 0.5s;
-	&:hover {
-		transition: 0.5s;
-		font-weight: bold;
-		text-transform: uppercase;
-		text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
-			0.025em 0.04em 0 #fffc00;
-		animation: glitch 1000ms infinite;
-		@keyframes glitch {
-			0% {
-				text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
-					0.025em 0.04em 0 #fffc00;
-			}
-			15% {
-				text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
-					0.025em 0.04em 0 #fffc00;
-			}
-			16% {
-				text-shadow: -0.05em -0.025em 0 #00fffc,
-					0.025em 0.035em 0 #fc00ff, -0.05em -0.05em 0 #fffc00;
-			}
-			49% {
-				text-shadow: -0.05em -0.025em 0 #00fffc,
-					0.025em 0.035em 0 #fc00ff, -0.05em -0.05em 0 #fffc00;
-			}
-			50% {
-				text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff,
-					0 -0.04em 0 #fffc00;
-			}
-			99% {
-				text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff,
-					0 -0.04em 0 #fffc00;
-			}
-			100% {
-				text-shadow: -0.05em 0 0 #00fffc, -0.025em -0.04em 0 #fc00ff,
-					-0.04em -0.025em 0 #fffc00;
-			}
+	@media (max-width: 600px) {
+		font-size: 3rem;
+	}
+`
+
+const IntroNextContainer = styled.div`
+	text-align: center;
+	font-size: 8rem;
+	transition: 0.5s;
+	text-transform: uppercase;
+	animation: glitch 900ms infinite;
+	@keyframes glitch {
+		0% {
+			text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
+				0.025em 0.04em 0 #8c56d2;
 		}
+		15% {
+			text-shadow: 0.05em 0 0 #00fffc, -0.03em -0.04em 0 #fc00ff,
+				0.025em 0.04em 0 #8c56d2;
+		}
+		16% {
+			text-shadow: -0.05em -0.025em 0 #00fffc, 0.025em 0.035em 0 #fc00ff,
+				-0.05em -0.05em 0 #8c56d2;
+		}
+		49% {
+			text-shadow: -0.05em -0.025em 0 #00fffc, 0.025em 0.035em 0 #fc00ff,
+				-0.05em -0.05em 0 #8c56d2;
+		}
+		50% {
+			text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff,
+				0 -0.04em 0 #8c56d2;
+		}
+		99% {
+			text-shadow: 0.05em 0.035em 0 #00fffc, 0.03em 0 0 #fc00ff,
+				0 -0.04em 0 #8c56d2;
+		}
+		100% {
+			text-shadow: -0.05em 0 0 #00fffc, -0.025em -0.04em 0 #fc00ff,
+				-0.04em -0.025em 0 #8c56d2;
+		}
+	}
+	@media (max-width: 600px) {
+		font-size: 4rem;
 	}
 `
 
 const MePicture = styled.div`
+	width: 550px;
+	height: 500px;
+	position: fixed;
+	bottom: 0;
+	left: 0;
 	background-image: url(${imageMe});
 	background-repeat: no-repeat;
-	background-size: 40vw;
+	background-size: 600px;
 	background-position-y: 103%;
 	background-position-x: -2%;
-	height: 100vh;
-	filter: brightness(50%);
 	transition: 1s;
 	&:hover {
-		filter: brightness(100%);
 		background-position-y: 100%;
 		background-position-x: 0%;
 	}
 `
+
 interface HeaderContainerProps {
-	readonly shadow: boolean
+	readonly shadow: string
 }
 
 const HeaderContainer = styled.header<HeaderContainerProps>`
 	z-index: 99;
 	transition: 0.5s;
-	width: 1000px;
 	position: fixed;
-	top: 10%;
+	top: 5%;
 	left: 50%;
 	transform: translate(-50%, -50%);
-	padding: 20px 0 20px 0;
 	text-align: center;
 	display: flex;
 	flex-direction: row;
-	justify-content: space-evenly;
+	justify-content: space-between;
+	width: ${(props) => (props.shadow === 'yes' ? '50dvw' : '70dvw')};
+	padding: ${(props) => (props.shadow === 'yes' ? '1rem' : '0')};
 	align-items: center;
-	margin-top: -55px;
-	margin-top: ${(props) => (props.shadow ? '-55px' : '0')};
+	margin-top: ${(props) => (props.shadow === 'yes' ? '40px' : '0')};
 	background: ${(props) =>
-		props.shadow ? 'rgba(0, 0, 0, 0.167)' : 'transparent'};
-	border-radius: 80px;
+		props.shadow === 'yes' ? 'rgba(255, 255, 255, 0.154)' : 'transparent'};
+	border-radius: 70px 20px 70px 20px;
 	box-shadow: ${(props) =>
-		props.shadow ? '0 4px 30px rgba(0, 0, 0, 0.1)' : 'none'};
-	backdrop-filter: ${(props) => (props.shadow ? 'blur(7px)' : 'blur(0px)')};
+		props.shadow === 'yes' ? '0 4px 30px rgba(0, 0, 0, 0.1)' : 'none'};
+	backdrop-filter: ${(props) =>
+		props.shadow === 'yes' ? 'blur(7px)' : 'blur(0px)'};
+	p {
+		color: #8c56d2;
+	}
+	button {
+		background-color: ${(props) =>
+			props.shadow === 'yes' ? '#8c56d2' : 'transparent'};
+		color: ${(props) => (props.shadow === 'yes' ? '#373737' : '#8c56d2')};
+	}
+	button:hover {
+		background-color: ${(props) =>
+			props.shadow === 'yes' ? 'transparent' : '#8c56d2'};
+		color: ${(props) => (props.shadow === 'yes' ? '#8c56d2' : '#373737')};
+	}
+	@media (max-width: 1500px) {
+		width: 85dvw;
+	}
 `
 
 const LeftHeaderTextContainer = styled.div`
-	p {
-		font-size: 2rem;
+	a {
+		button {
+			font-size: 2rem;
+		}
 	}
-	display: flex;
-	:first-child {
-		margin: 0px 10px 0px 10px;
-	}
-	padding-right: 15%;
 `
+
 const RightHeaderTextContainer = styled.div`
 	a {
 		button {
 			font-size: 2rem;
 		}
 	}
-	padding-left: 18%;
-`
-
-const DarkModeSwapper = styled.span`
-	z-index: 99;
-	position: fixed;
-	right: 20px;
-	bottom: 20px;
-	cursor: pointer;
 `
 
 const Arrow = styled.a`
-	z-index: 2;
-	position: fixed;
-	left: 50%;
-	bottom: 10%;
+	position: absolute;
+	bottom: 5%;
+	right: 49%;
 	transform: translate(-50%, -50%);
 	cursor: pointer;
-	border: solid #ffc300;
+	border: solid #8c56d2;
 	border-width: 0 10px 10px 0;
-	display: inline-block;
-	padding: 3px;
-	width: 90px;
-	height: 90px;
+	width: 70px;
+	height: 70px;
 	transition: 0.5s;
-	&:hover {
-		scale: 1.05;
-	}
 	animation: float 2.5s ease-in-out infinite;
 	@keyframes float {
 		0% {
@@ -248,13 +233,12 @@ const CTAGH = styled.button`
 	}
 	z-index: 2;
 	position: fixed;
-	top: 20px;
-	right: 20px;
-	background-color: white;
-	color: black;
+	bottom: 30px;
+	right: 30px;
+	background-color: #8c56d2;
+	color: #fff;
 	&:hover {
-		background-color: transparent;
-		color: white;
+		scale: 1.05;
 	}
 `
 
