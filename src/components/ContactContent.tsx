cc1872205b691c1b755e2e76c51034f11272fd02
David PR <serallapdivad@gmail.com>
2023-11-30 14:25:51 +0000

    Complete de contact content

--- src/components/ContactContent.tsx
+++ src/components/ContactContent.tsx
@@ -1,36 +1,39 @@
 import { FC } from 'react'
 import styled from 'styled-components'
-import { NormalText } from '../shared_styling/Styled'
+import ContactCard from './ContactCard'
 
 const ContactContent: FC = () => {
 	return (
 		<>
 			<BodyContainer id='contactSection'>
-				<NormalText>Contact info</NormalText>
-				{/* <FirstSection className='firstSection'>
-				</FirstSection>
-				<SecondSection className='secondSection'></SecondSection>
-				<ThirdSection className='thirdSection'></ThirdSection>
-				<FourthSection className='fourthSection'></FourthSection> */}
+				<IntroSection />
+				<TextArea>
+					<ContactCard />
+				</TextArea>
 			</BodyContainer>
 		</>
 	)
 }
 
 const BodyContainer = styled.div`
-	height: 100vh;
+	height: 100dvh;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
 `
-// const FirstSection = styled.div`
-// 	height: 25%;
-// `
-// const SecondSection = styled.div`
-// 	height: 25%;
-// `
-// const ThirdSection = styled.div`
-// 	height: 25%;
-// `
-// const FourthSection = styled.div`
-// 	height: 25%;
-// `
+
+const TextArea = styled.div`
+	backdrop-filter: blur(2px);
+	width: 60dvw;
+	padding-top: 10dvh;
+	@media (max-width: 1200px) {
+		padding-top: 0;
+		width: 100%;
+	}
+`
+
+const IntroSection = styled.div`
+	height: 150px;
+`
 
 export default ContactContent
