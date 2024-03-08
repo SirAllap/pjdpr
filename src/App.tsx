971ebe663f094d30cc23db43ae09d594d368c317
David PR <serallapdivad@gmail.com>
2024-03-08 20:30:53 +0100

    added the ExperienceContent to the entry component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import Intro from './components/Intro'
 import ProjectContent from './components/ProjectContent'
 import ContactContent from './components/ContactContent'
+import ExperienceContent from './components/ExperienceContent'
 
 const App: FC = () => {
 	return (
 		<>
 			<Intro />
 			<ProjectContent />
+			<ExperienceContent />
 			<ContactContent />
 		</>
 	)
